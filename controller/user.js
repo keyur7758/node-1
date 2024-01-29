dbb458dc74b07b636a680539c52a91fb84b3430b
keyur7758 <keyurk397@gmail.com>
2024-01-29 13:47:02 +0530

    node-1

--- controller/user.js
+++ controller/user.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const index = fs.readFileSync('index.html', 'utf-8')
+// const index = fs.readFileSync('index.html', 'utf-8')
 const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../data.json'), 'utf-8'))
 const users = data.users
 
